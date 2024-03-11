7389f3da63d478031f93621a6a373576b770c347
Om Mishra <contact@om-mishra.com>
2024-03-11 13:46:44 +0530

    Updates

--- Lab_2/Test_Automation_2.py
+++ Lab_2/Test_Automation_2.py
@@ -4,12 +4,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# UI Testing
 
 driver = webdriver.Chrome()
-driver.get("https://www.python.org/")
+driver.get("https://www.amazon.in/")
 print(driver.title)
 
-search_bar = driver.find_element("name","q")
+search_bar = driver.find_element("id","twotabse`archtextbox")
 
 search_bar.clear()
 search_bar.send_keys("getting started with python")
